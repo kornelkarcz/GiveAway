b44f2b5e3d88f8fc99577ca490a178732c7c4f6a
kornelkarcz <kornel.karcz@outlook.com>
2019-03-13 12:19:53 +0100

    Add fake user class

---
+++ src/main/java/pl/coderslab/model/FakeUser.java
@@ -0,0 +1,65 @@
+package pl.coderslab.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class FakeUser {
+
+    private Long id;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String password;
+    private boolean superAdmin;
+
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public boolean isSuperAdmin() {
+        return superAdmin;
+    }
+
+    public void setSuperAdmin(boolean superAdmin) {
+        this.superAdmin = superAdmin;
+    }
+
+
+}
