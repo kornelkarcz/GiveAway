e55338f5f8d8b2011d234a589f1822d6ec259f6b
kornelkarcz <kornel.karcz@outlook.com>
2019-03-13 11:09:14 +0100

    Create user class

---
+++ src/main/java/pl/coderslab/model/User.java
@@ -0,0 +1,99 @@
+package pl.coderslab.model;
+
+import org.hibernate.validator.constraints.NotBlank;
+import pl.coderslab.utils.BCrypt;
+
+import javax.persistence.*;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "users", uniqueConstraints = @UniqueConstraint(columnNames = "email"))
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "First name cannot be empty")
+    @Size(min = 3, max = 30, message = "Invalid first name size")
+    private String firstName;
+
+    @NotBlank(message = "Last name cannot be empty")
+    @Size(min = 2, max = 50, message = "Invalid last name size")
+    private String lastName;
+
+    @NotBlank(message = "Email name cannot be empty")
+    @Pattern(regexp = "[_a-zA-Z0-9-]+(\\.[_a-zA-Z0-9-]+)*@[a-zA-Z0-9-]+(\\.[a-zA-Z0-9-]+)*\\.([a-zA-Z]{2,}){1}", message = "Invalid email pattern")
+    private String email;
+
+    @NotBlank(message = "Password cannot be empty")
+    private String password;
+    
+    private boolean isAdmin;
+
+
+    public User() {
+    }
+
+    public User(Long id, String firstName, String lastName, String email, String password, boolean isAdmin) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.isAdmin = isAdmin;
+    }
+
+    public User(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = BCrypt.hashpw(password, BCrypt.gensalt());
+    }
+
+    public boolean isAdmin() {
+        return isAdmin;
+    }
+
+    public void setAdmin(boolean admin) {
+        isAdmin = admin;
+    }
+}
