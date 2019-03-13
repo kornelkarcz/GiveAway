410ddb1793733bc638c433b7b272559609590ce4
kornelkarcz <kornel.karcz@outlook.com>
2019-03-13 11:34:30 +0100

    Create user repo and service

---
+++ src/main/java/pl/coderslab/service/UserService.java
@@ -0,0 +1,29 @@
+package pl.coderslab.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import pl.coderslab.model.User;
+import pl.coderslab.repository.UserRepo;
+
+import java.util.List;
+
+@Component
+@Transactional
+public class UserService {
+
+    @Autowired
+    private UserRepo userRepo;
+
+    public void registerUser(User user) {
+        userRepo.save(user);
+    }
+
+    public User findById(Long id) {
+        return userRepo.findOne(id);
+    }
+
+    public List<User> findAllUsers() {
+        return userRepo.findAll();
+    }
+}
