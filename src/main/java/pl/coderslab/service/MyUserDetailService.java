59c48cfaf4c6e93654a0121929b3adc491d43b46
kornelkarcz <kornel.karcz@outlook.com>
2019-04-13 11:19:54 +0200

    Update user details service;
    ;

--- src/main/java/pl/coderslab/service/MyUserDetailService.java
+++ src/main/java/pl/coderslab/service/MyUserDetailService.java
@@ -1,29 +1,44 @@
 package pl.coderslab.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import pl.coderslab.model.MyUserDetails;
+import pl.coderslab.model.Role;
 import pl.coderslab.model.User;
 import pl.coderslab.repository.UserRepo;
 
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
 public class MyUserDetailService implements UserDetailsService {
 
-    private final UserService userService;
+    private UserService userService;
+
+    public void setUserRepository(UserService userService) {
+        this.userService = userService;
+    }
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         User user = userService.findByEmail(email);
-        if (Objects.isNull(user)) {
+        if (user == null)){
             throw new UsernameNotFoundException(email);
         }
-        return new MyUserDetails(user);
+
+        Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
+
+        for (Role role : user.getRoleSet()) {
+            grantedAuthorities.add(new SimpleGrantedAuthority(role.getName()));
+        }
+        return new CurrentUser(user.getEmail(), user.getPassword(), grantedAuthorities, user);
     }
 
 }
