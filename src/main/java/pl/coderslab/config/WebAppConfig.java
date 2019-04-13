31d1111a737f239b7d7a3d501acc7e1b26371b5b
kornelkarcz <kornel.karcz@outlook.com>
2019-04-13 12:11:48 +0200

    Add WebAppConfig config file

---
+++ src/main/java/pl/coderslab/config/WebAppConfig.java
@@ -0,0 +1,15 @@
+package pl.coderslab.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class WebAppConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/403").setViewName("403");
+        registry.addViewController("/admin").setViewName("admin");
+    }
+}
